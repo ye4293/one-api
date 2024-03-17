cdd086c6d328056fccc714a86ac38612b8c594e4
Ghostz <ye4293@gmail.com>
2024-03-18 00:51:23 +0800

    xx

--- controller/user.go
+++ controller/user.go
@@ -6,12 +6,13 @@ import (
 	"net/http"
 	"strconv"
 	"time"
-	"github.com/songquanpeng/one-api/common/config"
-	"github.com/songquanpeng/one-api/common/helper"
-	"github.com/songquanpeng/one-api/model"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
+	"github.com/songquanpeng/one-api/common"
+	"github.com/songquanpeng/one-api/common/config"
+	"github.com/songquanpeng/one-api/common/helper"
+	"github.com/songquanpeng/one-api/model"
 )
 
 type LoginRequest struct {
