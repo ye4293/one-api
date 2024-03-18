703e48e4d7b59841665377c99a36218fa2d55691
Ghostz <ye4293@gmail.com>
2024-03-18 20:07:18 +0800

    aa

--- controller/pay.go
+++ controller/pay.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/songquanpeng/one-api/common/config"
+	"github.com/songquanpeng/one-api/common/logger"
 	"github.com/songquanpeng/one-api/common/message"
 	"github.com/songquanpeng/one-api/model"
 )
@@ -46,31 +47,39 @@ func GetPayChannel(c *gin.Context) {
 func CryptCallback(c *gin.Context) {
 	var response model.CryptCallbackResponse
 	if err := c.ShouldBindQuery(&response); err != nil {
+		logger.SysLog("failed to binf query")
 		c.String(http.StatusUnauthorized, err.Error())
+		return
 	}
 	err := model.HandleCryptCallback(response)
 	if err != nil {
+		logger.SysLog("failed to handle callback")
 		c.String(http.StatusUnauthorized, err.Error())
+		return
 	}
 	userId := response.UserId
 	addAmount := response.ValueCoin
-	if response.Result == "3" {
-		err := model.IncreaseUserQuota(userId, int64(addAmount*500000))
-		if err != nil {
-			return
-		}
-		//send email and back message
-		email, err := model.GetUserEmail(userId)
-		if err != nil {
-			return
-		}
-		subject := fmt.Sprintf("%s's recharge notification email", config.SystemName)
-		content := fmt.Sprintf("<p>hello,You have successfully recharged %f$</p>"+"<p>Congratulations on getting one step closer to the AI world!</p>", addAmount)
-		err = message.SendEmail(subject, email, content)
-		c.JSON(http.StatusOK, gin.H{
-			"success": true,
-			"message": "",
-		})
+	err = model.IncreaseUserQuota(userId, int64(addAmount*500000))
+	if err != nil {
+		logger.SysLog("failed to increase user quote")
+		return
 	}
+	//send email and back message
+	email, err := model.GetUserEmail(userId)
+	if err != nil {
+		logger.SysLog("failed to get user email")
+		return
+	}
+	subject := fmt.Sprintf("%s's recharge notification email", config.SystemName)
+	content := fmt.Sprintf("<p>hello,You have successfully recharged %f$</p>"+"<p>Congratulations on getting one step closer to the AI world!</p>", addAmount)
+	err = message.SendEmail(subject, email, content)
+	if err != nil {
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"message": "",
+	})
+
 	c.String(http.StatusOK, "ok")
 }
