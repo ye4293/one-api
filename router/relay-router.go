9cade740f792ad5ef455ea1e6ded12f50d6c2918
Ghostz <ye4293@gmail.com>
2024-04-02 19:55:58 +0800

    fix error

--- router/relay-router.go
+++ router/relay-router.go
@@ -71,10 +71,6 @@ func SetRelayRouter(router *gin.Engine) {
 
 	relayMjRouter := router.Group("/mj")
 	relayMjRouter.GET("/image/:id", controller.RelayMidjourneyImage)
-	relayMjRouter.Use(middleware.Distribute())
-	{
-		relayMjRouter.POST("/notify", controller.RelayMidjourney)
-	}
 	relayMjRouter.Use(middleware.TokenAuth(), middleware.Distribute())
 	{
 		relayMjRouter.POST("/submit/action", controller.RelayMidjourney)
@@ -85,6 +81,7 @@ func SetRelayRouter(router *gin.Engine) {
 		relayMjRouter.POST("/submit/simple-change", controller.RelayMidjourney)
 		relayMjRouter.POST("/submit/describe", controller.RelayMidjourney)
 		relayMjRouter.POST("/submit/blend", controller.RelayMidjourney)
+		relayMjRouter.POST("/notify", controller.RelayMidjourney)
 		relayMjRouter.GET("/task/:id/fetch", controller.RelayMidjourney)
 		relayMjRouter.GET("/task/:id/image-seed", controller.RelayMidjourney)
 		relayMjRouter.POST("/task/list-by-condition", controller.RelayMidjourney)
