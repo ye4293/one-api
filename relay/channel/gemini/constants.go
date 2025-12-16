030a35ba33b4721becd1b2228c46e45e89d9ae49
ye4293 <ye4293@gmail.com>
2025-12-16 11:06:25 +0800

    gemini修改

--- relay/channel/gemini/constants.go
+++ relay/channel/gemini/constants.go
@@ -5,9 +5,18 @@ import "github.com/songquanpeng/one-api/relay/model"
 // https://ai.google.dev/models/gemini
 
 var ModelList = []string{
-	"gemini-1.5-flash", "gemini-1.5-pro", "gemini-1.5-pro-002", "gemini-2.0-flash", "gemini-2.5-flash", "gemini-2.5-flash-lite", "gemini-2.5-pro", "gemini-2.5-flash-image-preview",
+	// Gemini 1.5 系列
+	"gemini-1.5-flash", "gemini-1.5-pro", "gemini-1.5-pro-002",
+	// Gemini 2.0 系列
+	"gemini-2.0-flash",
+	// Gemini 2.5 系列
+	"gemini-2.5-flash", "gemini-2.5-flash-lite", "gemini-2.5-pro", "gemini-2.5-flash-image-preview", "gemini-2.5-flash-image",
 	"gemini-2.5-flash-thinking", "gemini-2.5-pro-thinking", "gemini-2.5-flash-lite-thinking",
-	"gemini-2.5-flash-nothinking", "gemini-2.5-flash-lite-thinking", "gemini-2.5-pro-nothinking", "gemini-2.5-flash-image", "gemini-3-pro-preview", "gemini-3-pro-preview-thinking", "gemini-3-pro-image-preview",
+	"gemini-2.5-flash-nothinking", "gemini-2.5-flash-lite-nothinking", "gemini-2.5-pro-nothinking", "gemini-2.5-flash-lite-preview-09-2025-nothinking", "gemini-2.5-flash-preview-09-2025-nothinking",
+	"gemini-2.5-flash-lite-preview-09-2025", "gemini-2.5-flash-lite-preview-09-2025-thinking", "gemini-2.5-flash-preview-09-2025", "gemini-2.5-flash-preview-09-2025-thinking",
+
+	// Gemini 3 系列
+	"gemini-3-pro-preview", "gemini-3-pro-preview-thinking", "gemini-3-pro-image-preview",
 }
 
 var ModelDetails = []model.APIModel{
